e30a5c382d0b3a44eeafa3855dc6160bf7a37a6a
luka <120731202+poladaa@users.noreply.github.com>
2023-08-07 18:15:37 +0400

    Add files via upload

---
+++ travel-list/src/Stats.js
@@ -0,0 +1,20 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items to your packing list 👜</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+  return (
+    <footer className="stats">
+      <p>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </p>
+    </footer>
+  );
+}
